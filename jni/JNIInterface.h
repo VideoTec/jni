e54146b38982889a1be33f6d51d7ac0c8c368e33
wx <wx@qq.com>
2015-01-12 17:50:55 +0800

    c extern staic int internal linkage

--- jni/JNIInterface.h
+++ jni/JNIInterface.h
@@ -13,4 +13,9 @@
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
+#define HELLOWORLD_JNI_CLS "com/example/jnihelloworld/JNIInterface"
+
+extern const int g_methods_num;
+extern JNINativeMethod g_helloworl_jnicls_methods[];
+
 #endif //_H_JNI_INTERFACE_H_
