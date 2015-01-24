723d4aeb978dff7b0cc306d5d5a6b8f41a335266
wx <wx@qq.com>
2015-01-24 18:11:48 +0800

    JNIEXPORT AND JNICALL

--- jni/hello_world.cpp
+++ jni/hello_world.cpp
@@ -1,16 +1,16 @@
 #include "jni_util.h"
 
 extern "C" {
-
-void helloworld(JNIEnv *env, jobject clazz, jstring jni_str) {
-  const char *str_utf8 = env->GetStringUTFChars(jni_str, nullptr);
-  LOGI("%s", str_utf8);
-  env->ReleaseStringUTFChars(jni_str, str_utf8);
-}
-
+JNIEXPORT void JNICALL helloworld(JNIEnv *env, jobject clazz, jstring jni_str);
 }
 
 JNINativeMethod g_helloworld_methods[] = {
     {"say_hello", "(Ljava/lang/String;)V", (void *)helloworld}
 };
 extern "C" const int g_helloworld_methods_num = sizeof(g_helloworld_methods) / sizeof(g_helloworld_methods[0]);
+
+void helloworld(JNIEnv *env, jobject clazz, jstring jni_str) {
+  const char *str_utf8 = env->GetStringUTFChars(jni_str, nullptr);
+  LOGI("%s", str_utf8);
+  env->ReleaseStringUTFChars(jni_str, str_utf8);
+}
