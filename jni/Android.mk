16b556f1cde659bb006abea71b931485dd20367d
wx <wangxiangc@gmail.com>
2015-05-22 22:52:52 +0800

    change app_abi and add no-undefined waring

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := helloworld
 LOCAL_SRC_FILES := jni_entry.c hello_world.cpp
 LOCAL_STATIC_LIBRARIES := avfilter avformat swresample swscale avcodec avutil opencore-amrnb x264
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
-LOCAL_CFLAGS := -std=c99 -D__STDC_CONSTANT_MACROS
+LOCAL_CFLAGS := -std=c99 -D__STDC_CONSTANT_MACROS -Wl,--no-undefined
 LOCAL_CPPFLAGS := -std=c++11
 LOCAL_CPP_FEATURES := rtti exceptions
 LOCAL_LDLIBS := -llog -ljnigraphics -landroid -lz -lGLESv2 -lEGL -lOpenSLES
