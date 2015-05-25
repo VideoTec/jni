4a2c9a6d56283a7e4abeb7d58e4743ffe11a359d
wx <wangxiangc@gmail.com>
2015-05-25 20:05:59 +0800

    change copyso.bat

--- copyso.bat
+++ copyso.bat
@@ -2,12 +2,14 @@
 cd %~dp0
 :BUILD
 call ndk-build V=1 NDK_DEBUG=0
+echo 编译结束
 :WAIT
 set /p KEY=
 cls
 if "%KEY%"=="C" (
   call ndk-build clean
-  set KEY=
+  echo 清理结束
+  set KEY=0
   goto WAIT
 
 ) else (
