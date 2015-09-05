0934df5044c19cb663fe17bac11cdba1ddc237bc
wx <wangxiangc@gmail.com>
2015-09-05 18:50:40 +0800

    add jni throw exception util fuc

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cxx .cpp .cc
 LOCAL_MODULE := jniTemplate
-LOCAL_SRC_FILES := jni_entry.c hello_world.cpp
+LOCAL_SRC_FILES := jni_util.c jni_entry.c hello_world.cpp
 LOCAL_CFLAGS := -D__STDC_CONSTANT_MACROS -Wl,--no-undefined
 LOCAL_CPPFLAGS := -std=c++11
 LOCAL_CONLYFLAGS := -std=c99
